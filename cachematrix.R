dd1aac2ad2a201211fe5c11cc64b0c4659f06591
Pier Lorenzo  Paracchini <pierlorenzo.paracchini@gmail.com>
2014-12-13 13:07:44 +0100

    added functionality to makeCacheMatrix function: set, get, setinverse, getinverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,32 @@
 ## makeCacheMatrix and cacheSolve functions allow to cache a costly
 ## computation, the inversion of the provided Matrix. 
 
-## makeCacheMatrix creates a special "matrix" object that can cache
-## its inverse. Assumption: provided matrix is invertible.
+## makeCacheMatrix creates a special "matrix" object from the provided matrix
+## that can cache its inverse. Assumption: provided matrix is invertible.
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        cached_inverse <- NULL # the cached inverse of the provided matrix
+        
+        # set: change the matrix
+        # Setting a new matrix the cached inverse of the matrix is reset to NULL 
+        set <- function(y){
+                x <<- y # set x to new provided matrix
+                cached_inverse <<- NULL # clear cached inverse
+        }
+        
+        # get: get the matrix
+        get <- function() x # return x (matrix)
+        
+        # setinverse: set the inverse of the matrix
+        setinverse <- function(inverse){
+                cached_inverse <<- inverse 
+        }
+        
+        # getinverse: get inverse of the matrix
+        getinverse <- function() cached_inverse
+        
+        return(list(set = set, get = get, setinverse = setinverse, getinverse = getinverse))
 
 }
 
